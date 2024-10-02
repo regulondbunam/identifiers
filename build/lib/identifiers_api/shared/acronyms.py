18d1bdb1e75dc4c1a6d279c8e27acaa48cae374f
Phill Betancourt <Felipe.Bet@Hotmail.com>
2024-10-02 17:22:43 -0600

    UPDATE: Removing intallation files

--- build/lib/identifiers_api/shared/acronyms.py
+++
@@ -1,8 +0,0 @@
-from identifiers_api.shared.constants import *
-
-
-def get_db_acronym(identifiers_object):
-    database = identifiers_object.get("regulondbDatabase", None)
-    if database in REGULONDB_DATABASES_ACRONYMS:
-        return REGULONDB_DATABASES_ACRONYMS[database]
-    return "RDB"
