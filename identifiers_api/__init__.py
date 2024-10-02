55011a777f0084bbb071409fc85f5c98bba2823f
Phill Betancourt <Felipe.Bet@Hotmail.com>
2024-10-02 17:17:51 -0600

    UPDATE: GC Datamarts

--- identifiers_api/__init__.py
+++ identifiers_api/__init__.py
@@ -6,6 +6,7 @@ from identifiers_api.services.identifiers import get_identifiers
 from identifiers_api.services.identifiers import update_id
 from identifiers_api.services import regulondbmultigenomic
 from identifiers_api.services import regulondbht
+from identifiers_api.services import regulondbdatamarts
 
 
 def connect(uri, database="regulondbidentifiers"):
--- identifiers_api/shared/constants.py
+++ identifiers_api/shared/constants.py
@@ -1,6 +1,7 @@
 REGULONDB_DATABASES_ACRONYMS = {
     "regulondbmultigenomic": "RDB",
-    "regulondbht": "RHT"
+    "regulondbht": "RHT",
+    "regulondbdatamarts": "RDM"
 }
 
 RDB_MULTIGENOMIC_SUBCLASS_ACRONYMS = {
@@ -87,5 +88,6 @@ ONTOLOGY_SUBCLASS_ACRONYMS = {
 
 # TODO: Ver con Gabo y Edgar
 DATAMARTS_ACRONYMS = {
-    "overviews": ""
+    "overviews": "",
+    "growthCondition": "GCC"
 }
